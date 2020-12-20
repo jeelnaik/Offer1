a87499deb1428b148b4fd7308072844b06aed40d
Jeel <jeel.naik@permitdocs.com>
2020-12-20 14:41:31 -0800

    MVP Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Homes from './pages/Homes'
+import Home from './pages/Home'
+import Message from './pages/Message'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/homes" component={Homes} />
+            <Route exact path="/" component={Homes} />
+            <Route exact path="/home/:homeId" component={Home} />
+            <Route exact path="/home/send" component={Message} />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
